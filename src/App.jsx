ff1a3f3b1b1ad884f14ee039f58c00c87e60eaa8
qwezd <qwezd8074@gmail.com>
2025-10-01 22:03:23 +0900

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <></>;
+  const [id, setId] = useState("");
+  const [pass, setPass] = useState("");
+
+  function handleLogin() {
+    // TODO: 로그인 요청 로직 구현 필요
+    alert("로그인")
+  }
+  
+  useEffect(()=>{
+    console.log(id);
+  }, [id])
+
+  useEffect(()=>{
+   console.log(pass);
+  }, [pass])
+
+  return (
+<div>
+
+  id: <input onChange={(e)=>{setId(e.target.value)}}></input>
+  <br></br>
+  pass: <input onChange={(e)=>{setPass(e.target.value)}}></input>
+  <br></br>
+  <button onClick={login}>login</button>
+</div>
+  )
 }
 
 export default App;
