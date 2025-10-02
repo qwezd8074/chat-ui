da67316fa3be7371919af9b81045fe2080d86ba6
qwezd <qwezd8074@gmail.com>
2025-10-02 19:58:29 +0900

    백엔드랑 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
+import axios from "axios";
 import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [id, setId] = useState("");
+  const [name, setName] = useState("");
   const [pass, setPass] = useState("");
 
-  function handleLogin() {
+  async function handleLogin() {
     // TODO: 로그인 요청 로직 구현 필요
-    alert("로그인")
-  }
-  
-  useEffect(()=>{
-    console.log(id);
-  }, [id])
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("pass", pass);
 
-  useEffect(()=>{
-   console.log(pass);
-  }, [pass])
+    await axios
+      .post("http://localhost:8080/login", formData, {
+        withCredentials: true, // 쿠키를 함께 보내도록 설정
+      })
+      .then((response) => {
+        // response 안에 데이터가 들어있음
+        console.log(response.data); // 실제 응답 데이터
+        alert(response.data);
+      })
+      .catch((error) => {
+        console.error("에러 발생:", error);
+        alert(error.response.data);
+      });
+  }
+
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
+
+  useEffect(() => {
+    console.log(pass);
+  }, [pass]);
 
   return (
-<div>
-
-  id: <input onChange={(e)=>{setId(e.target.value)}}></input>
-  <br></br>
-  pass: <input onChange={(e)=>{setPass(e.target.value)}}></input>
-  <br></br>
-  <button onClick={login}>login</button>
-</div>
-  )
+    <main className={"wrapper"}>
+      <div className={"box"}>
+        <div className={"input-group"}>
+          <label>ID</label>
+          <input
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div className={"input-group"}>
+          <label>PASS</label>
+          <input
+            onChange={(e) => {
+              setPass(e.target.value);
+            }}
+          />
+        </div>
+        <div className={"button-group"}>
+          <button onClick={handleLogin}>login</button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
